449e1253d9dc6865825726132743212c5be66494
Domey Benjamin <domeybenjamin1@gmail.com>
2023-07-11 17:03:13 +0000

    chore: updated port

--- release/main.tf
+++ release/main.tf
@@ -71,5 +71,5 @@ resource "google_compute_firewall" "flask" {
 # Add an output variable for the web server URL
 # A variable for extracting the external IP address of the VM
 output "Web-server-URL" {
- value = join("",["http://",google_compute_instance.default.network_interface.0.access_config.0.nat_ip,":5000"])
+ value = join("",["http://",google_compute_instance.default.network_interface.0.access_config.0.nat_ip,":5001"])
 }
